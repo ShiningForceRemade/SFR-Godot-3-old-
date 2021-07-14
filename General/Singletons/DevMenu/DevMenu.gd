7377065c6807e437c4e2c9626cd8b604db51af53
feldc <feldc@protonmail.com>
2021-07-14 02:21:52 -0400

    Add: pressing tilda takes in game screenshot (saved to res screenshots)

--- General/Singletons/DevMenu/DevMenu.gd
+++ General/Singletons/DevMenu/DevMenu.gd
@@ -12,3 +12,23 @@ func _input(event):
 		OS.window_fullscreen = !OS.window_fullscreen
 	
 	# TODO: add dev menu and perform check here
+	
+	if event.is_action_pressed("ui_tilda"):
+		print("Saving Screenshot")
+		
+		var time = OS.get_datetime()
+		var time_string = str(time["year"]) + "-" + str(time["month"]) + "-" + str(time["day"]) + "_H" + str(time["hour"]) + "M" + str(time["minute"]) + "S" + str(time["second"])
+		
+		var img = get_viewport().get_texture().get_data()
+		
+		yield(get_tree(), "idle_frame")
+		yield(get_tree(), "idle_frame")
+		
+		img.flip_y()
+		
+		var save_path = "res://Screenshots/" + time_string + ".png"
+		
+		img.save_png(str(save_path))
+		
+		# TODO: add some kind of ingame message or popup to notify a screenshot was saved
+
