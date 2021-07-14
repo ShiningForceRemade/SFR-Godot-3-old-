3599ffd9a966d1bcd0a8826105b608e81651601d
feldc <feldc@protonmail.com>
2021-07-14 02:04:18 -0400

    Add: fullscreen toggle (f11)

--- General/Singletons/DevMenu/DevMenu.gd
+++ General/Singletons/DevMenu/DevMenu.gd
@@ -4,7 +4,11 @@ func _ready():
 	pass
 
 func _input(event):
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_released("ui_cancel"):
 		get_tree().paused = !get_tree().paused
+	
+	if event.is_action_released("ui_toggle_fullscreen"):
+		# print("Toggled Fullscreen")
+		OS.window_fullscreen = !OS.window_fullscreen
 	
 	# TODO: add dev menu and perform check here
