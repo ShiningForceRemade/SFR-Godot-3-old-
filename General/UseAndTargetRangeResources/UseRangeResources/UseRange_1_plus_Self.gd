0c0910cfbebf453103aa68ee6641f59d5e7c6983
feldc <feldc@protonmail.com>
2021-07-19 01:13:10 -0400

    Add: more use range and selection logic (WIP)

---
+++ General/UseAndTargetRangeResources/UseRangeResources/UseRange_1_plus_Self.gd
@@ -0,0 +1,78 @@
+extends CN_SF_UseRange
+
+const tile_size: int = 24
+const half_tile: int = tile_size / 2
+
+func _ready():
+	print("In here not base")
+	pass
+
+static func draw_use_range():
+	# print("UseRange_1 - vpos - ", vposz, " FieldLogicNode - ", fieldLogicNode)
+	var uttwn = Singleton_Game_GlobalBattleVariables.field_logic_node.get_node("UseTargetTilesWrapperNode")
+	
+	for n in uttwn.get_children():
+		uttwn.remove_child(n)
+		n.queue_free()
+	
+	var vpos = Singleton_Game_GlobalBattleVariables.currently_active_character.position
+	vpos.x -= half_tile
+	vpos.y -= half_tile
+	
+	# center tile
+	Singleton_Game_GlobalBattleVariables.field_logic_node.draw_flashing_movement_square(
+		Color.purple,
+		vpos.x,
+		vpos.y,
+		uttwn
+	)
+	
+	# Top tile
+	Singleton_Game_GlobalBattleVariables.field_logic_node.draw_flashing_movement_square(
+		Color.purple,
+		vpos.x,
+		vpos.y - tile_size,
+		uttwn
+	)
+
+	# Bottom tile
+	Singleton_Game_GlobalBattleVariables.field_logic_node.draw_flashing_movement_square(
+		Color.purple,
+		vpos.x,
+		vpos.y + tile_size,
+		uttwn
+	)
+
+	# Left tile
+	Singleton_Game_GlobalBattleVariables.field_logic_node.draw_flashing_movement_square(
+		Color.purple,
+		vpos.x - tile_size,
+		vpos.y,
+		uttwn
+	)
+
+	# Right tile
+	Singleton_Game_GlobalBattleVariables.field_logic_node.draw_flashing_movement_square(
+		Color.purple,
+		vpos.x + tile_size,
+		vpos.y,
+		uttwn
+	)
+
+	# X 1 X
+	# 1 X 1
+	# X 1 X
+#	return [
+#		[null, 1,    null],
+#		[1,    null, 1],
+#		[null, 1,    null],
+#		]
+	print("\n")
+	
+
+func get_use_range_array_representation() -> Array:
+	return [
+		[null,    1, null],
+		[1,       1,    1],
+		[null,    1, null],
+	]
--- SF1/Chapters/1/TargetSelectionLogicNodeRoot.gd
+++ SF1/Chapters/1/TargetSelectionLogicNodeRoot.gd
@@ -8,16 +8,6 @@ var is_target_selection_active: bool = false
 
 var target_node_children
 
-# NOTE: testing with fixed use case
-# TODO: create a generic logic node higher up in the tree to handle target range type and  use range type
-# save the following function logic in the battle singleton and let that handle the following actions the the logic node completes its selection after
-# 
-var use_range = [
-	[null, 1, null],
-	[1,    1,    1],
-	[null, 1, null]
-	]
-
 # very messy clean this up later 
 # TODO: redo global battle vars to track more of this state internally
 # so there isn't so much repatition in various nodes that are mostly similar
@@ -25,10 +15,27 @@ var target_range
 
 var item_use_range
 
+var current_selection_vec2
+
 	
 # var active_char_root = Singleton_Game_GlobalBattleVariables.currently_active_character.get_node("CharacterRoot")
 	
 
+var use_range_array_rep
+var ac_pos
+	
+# total rows and columns
+var t_rows
+var t_cols
+	
+# center column and row
+var c_col
+var c_row
+	
+var row_tiles_movement
+var col_tiles_movement
+	
+var check_pos = Vector2(0, 0)
 
 func _ready():
 	Singleton_Game_GlobalBattleVariables.target_selection_node = self
@@ -38,120 +45,34 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
 func _input(event):
-#	if is_battle_use_menu_active:
-#		if event.is_action_released("ui_b_key"):
-#			print("Cancel Use Inventory Menu")
-#			is_battle_use_menu_active = false
-#			# Singleton_Game_GlobalBattleVariables.currently_active_character.get_node("CharacterRoot").active = true
-#			# get_parent().get_parent().get_parent().s_hide_battle_inventory_menu()
-#			get_parent().get_parent().get_parent().s_hide_battle_use_menu()
-#
-#			# TODO: HACK: FIXME: Dirty hack need a better way to gurantee when action is completed to prevent retrigger
-#			# yield on signal seems busted sometimes gets double called or falls through?
-#			yield(get_tree().create_timer(0.1), "timeout")
-#			get_parent().get_parent().get_parent().s_show_battle_inventory_menu("right")
-#			# get_parent().get_parent().get_parent().s_show_battle_inventory_menu()
-#			# get_parent().get_node("BattleInventoryMenuRoot").set_battle_inventory_menu_active()
-#			return
-#
-#		if event.is_action_released("ui_accept"):
-#			print("Accept Action - ", currently_selected_option)
-#
-#			var actor = Singleton_Game_GlobalBattleVariables.currently_active_character.get_node("CharacterRoot")
-#
-#			print(actor.inventory_items_id[currently_selected_option].item_name)
-#			print(actor.is_item_equipped[currently_selected_option])
-#
-#			# actor.inventory_items_id[currently_selected_option].item_use_range_path
-#			# actor.inventory_items_id[currently_selected_option].item_use_target_path
-#
-#			is_battle_use_menu_active = false
-#			is_target_selection_active = true
-#			Singleton_Game_GlobalBattleVariables.field_logic_node.hide_movement_tiles()
-#			Singleton_Game_GlobalBattleVariables.field_logic_node.show_use_target_tiles()
-#
-#			get_parent().get_parent().get_parent().s_hide_battle_use_menu()
-#			setup_use_range_and_target_range_selection(actor.inventory_items_id[currently_selected_option])
-#			yield(self, "signal_completed_item_use_action")
-#
-#			# todo if cancelled
-#			is_battle_use_menu_active = true
-#			is_target_selection_active = false
-#			get_parent().get_parent().get_parent().s_show_battle_use_menu()
-#			# if completed action
-#			# emit turn completed to field logic node
-#			# Singleton_Game_GlobalBattleVariables.field_logic_node
-#
-#			print("Complete")
-#			#if currently_selected_option == e_menu_options.STAY_OPTION:
-#			#	print("Currently Active Character Node - ", Singleton_Game_GlobalBattleVariables.currently_active_character)
-#			#	Singleton_Game_GlobalBattleVariables.currently_active_character.s_complete_turn()
-#
-#			#	# emit_signal("signal_completed_turn")
-#			#	return
-
 	if is_target_selection_active:
 		if event.is_action_released("ui_b_key"):
 			is_target_selection_active = false
-			emit_signal("signal_completed_item_use_action")
+			# emit_signal("signal_completed_item_use_action")
 			Singleton_Game_GlobalBattleVariables.battle_base.s_hide_target_actor_micro()
 			Singleton_Game_GlobalBattleVariables.field_logic_node.show_movement_tiles()
 			Singleton_Game_GlobalBattleVariables.field_logic_node.hide_use_target_tiles()
 			target_range.cleanup_cursor()
-			
+			Singleton_Game_GlobalBattleVariables.battle_base.s_show_character_action_menu()
+		
 		if event.is_action_released("ui_a_key"):
 			is_target_selection_active = false
 			print("Called")
 			print("TODO: trigger battle action scene and play out the item use effect")
 			Singleton_Game_GlobalBattleVariables.battle_base.s_hide_target_actor_micro()
 		
-		if event.is_action_pressed("ui_down"):
-			print("Called")
-			
-			var pos = Singleton_Game_GlobalBattleVariables.currently_active_character.position
-			var vpos = Vector2(pos.x, pos.y + 24)
-			var n = get_character_at_tile_position(vpos)
-			if n != null:
-				print("New Target Selection")
-				Singleton_Game_GlobalBattleVariables.currently_selected_actor = n
-				Singleton_Game_GlobalBattleVariables.battle_base.s_show_target_actor_micro()
-				target_range.draw_cursor_at_position(vpos)
-			
-			# is_target_selection_active = false
-			# emit_signal("signal_completed_item_use_action")
-		elif event.is_action_pressed("ui_up"):
-			var pos = Singleton_Game_GlobalBattleVariables.currently_active_character.position
-			get_character_at_tile_position(Vector2(pos.x, pos.y - 24))
-			
-			# is_target_selection_active = false
-			# emit_signal("signal_completed_item_use_action")
-		elif event.is_action_pressed("ui_right"):
-			var pos = Singleton_Game_GlobalBattleVariables.currently_active_character.position
-			var vpos = Vector2(pos.x + 24, pos.y)
-			var n = get_character_at_tile_position(vpos)
-			if n != null:
-				print("New Target Selection")
-				Singleton_Game_GlobalBattleVariables.currently_selected_actor = n
-				Singleton_Game_GlobalBattleVariables.battle_base.s_show_target_actor_micro()
-				target_range.draw_cursor_at_position(vpos)
-			
-			# is_target_selection_active = false
-			# emit_signal("signal_completed_item_use_action")
-		elif event.is_action_pressed("ui_left"):
-			var pos = Singleton_Game_GlobalBattleVariables.currently_active_character.position
-			get_character_at_tile_position(Vector2(pos.x - 24, pos.y))
-			
-			# is_target_selection_active = false
-			# emit_signal("signal_completed_item_use_action")
+		if event.is_action_pressed("ui_down") or event.is_action_pressed("ui_left"):
+			target_selection_counter_clockwise()
+		elif event.is_action_pressed("ui_right") or event.is_action_pressed("ui_up"):
+			target_selection_clockwise()
 		
 
 func setup_use_range_and_target_range_selection(item_arg) -> void:
+	Singleton_Game_GlobalBattleVariables.field_logic_node.hide_movement_tiles()
+	
 	is_target_selection_active = true
-	
-	print("Called")
-	
+
 	print(item_arg)
 	print(item_arg.item_use_range_path)
 	item_use_range = load(item_arg.item_use_range_path).new()
@@ -167,34 +88,21 @@ func setup_use_range_and_target_range_selection(item_arg) -> void:
 	
 	print("Target Actor Type - ", item_arg.target_actor_type)
 	if item_arg.target_actor_type == 4:
-		print("Self and Characters")
-		print(Singleton_Game_GlobalBattleVariables.character_wrapper_node)
-		
-		print(Singleton_Game_GlobalBattleVariables.currently_active_character)
-		print(Singleton_Game_GlobalBattleVariables.currently_active_character.position)
-		
-		print(Singleton_Game_GlobalBattleVariables.currently_selected_actor)
-		
+		print("Self and Characters", Singleton_Game_GlobalBattleVariables.character_wrapper_node)
 		target_node_children = Singleton_Game_GlobalBattleVariables.character_wrapper_node.get_children()
-		for child in target_node_children:
-			print(child)
-			print(child.position)
+		
 	elif item_arg.target_actor_type == 2: # Enemeies - TODO: add enum or consts to use instead of raw numbers
-		print("Enemies")
-		print(Singleton_Game_GlobalBattleVariables.enemies_wrapper_node)
-		
-		print(Singleton_Game_GlobalBattleVariables.currently_active_character)
-		print(Singleton_Game_GlobalBattleVariables.currently_active_character.position)
-		
-		print(Singleton_Game_GlobalBattleVariables.currently_selected_actor)
-		
+		print("Enemies", Singleton_Game_GlobalBattleVariables.enemies_wrapper_node)
 		target_node_children = Singleton_Game_GlobalBattleVariables.enemies_wrapper_node.get_children()
-		#for child in target_node_children:
-		#	print(child)
-		#	print(child.position)
-		
 		set_cursor_target_on_first_found_enemey()
+		
 		# target_range.draw_cursor_at_position(target_node_children[0].position)
+	
+	elif item_arg.target_actor_type == 3: # Enemeies - TODO: add enum or consts to use instead of raw numbers
+		print("Both", Singleton_Game_GlobalBattleVariables.enemies_wrapper_node)
+		target_node_children = Singleton_Game_GlobalBattleVariables.enemies_wrapper_node.get_children()
+		target_node_children += Singleton_Game_GlobalBattleVariables.character_wrapper_node.get_children()
+		set_cursor_target_on_first_found_enemey()	
 	#emit_signal("signal_completed_item_use_action")
 	# return
 
@@ -207,28 +115,8 @@ func get_character_at_tile_position(pos_arg):
 	return null
 
 # TODO: this can be simplified soooo much clean this up and remove a lot of the redundant elements later
-
 func set_cursor_target_on_first_found_enemey() -> void:
-	var use_range_array_rep = item_use_range.get_use_range_array_representation()
-	var ac_pos = Singleton_Game_GlobalBattleVariables.currently_active_character.position
-	
-	# total rows and columns
-	var t_rows = use_range_array_rep.size()
-	var t_cols = use_range_array_rep[0].size()
-	
-	# center column and row
-	var c_col = floor(t_cols / 2)
-	var c_row = floor(t_rows / 2)
-	
-	var row_tiles_movement = 0
-	var col_tiles_movement = 0
-	
-	var subtract_tiles_row = false
-	var subtract_tiles_col = false
-	
-	var check_pos = Vector2(0, 0)
-	var x 
-	var y
+	setup_quadrant_vars()
 	
 	for child in target_node_children:
 		print(child)
@@ -239,42 +127,8 @@ func set_cursor_target_on_first_found_enemey() -> void:
 				if use_range_array_rep[r][c] != null:
 					print(use_range_array_rep[r][c])
 					
-					if r < c_row:
-						row_tiles_movement = c_row - r
-						subtract_tiles_row = true
-					elif r > c_row:
-						row_tiles_movement = r - c_row
-						subtract_tiles_row = false
-					else:
-						row_tiles_movement = 0
-						subtract_tiles_row = false
-					
-					if c < c_col:
-						col_tiles_movement = c_col - c
-						subtract_tiles_col = true
-					elif c > c_col:
-						col_tiles_movement = c - c_col
-						subtract_tiles_col = false
-					else:
-						col_tiles_movement = 0
-						subtract_tiles_col = false
-					
-					if subtract_tiles_row:
-						y = row_tiles_movement * tile_size
-						y = y * -1
-					else:
-						y = row_tiles_movement * tile_size
-					
-					if subtract_tiles_col:
-						x = col_tiles_movement * tile_size
-						x = x * -1
-					else:
-						x = col_tiles_movement * tile_size
-							
-					if child.position == Vector2(ac_pos.x + x, ac_pos.y + y):
-						target_range.draw_cursor_at_position(child.position)
-						Singleton_Game_GlobalBattleVariables.currently_selected_actor = child
-						Singleton_Game_GlobalBattleVariables.battle_base.s_show_target_actor_micro()
+					if child.position == Vector2(ac_pos.x + get_tile_position_adjustment(r, c_row), ac_pos.y + get_tile_position_adjustment(c, c_col)):
+						set_and_save_new_target_selection(r, c, child)
 						# Found inital target no need to continue searching
 						return
 	
@@ -283,3 +137,293 @@ func set_cursor_target_on_first_found_enemey() -> void:
 	# TODO: probably should do this check much earlier
 	
 	return
+
+func target_selection_clockwise():
+	print("Clockwise")
+	setup_quadrant_vars()
+	print(c_row, current_selection_vec2.x, c_col, current_selection_vec2.y)
+	
+	if c_col <= current_selection_vec2.y:
+		if right_side_pass_vt(current_selection_vec2.x, current_selection_vec2.y):
+			return
+		if left_side_pass_upwards(t_rows - 1, c_col):
+			return
+		if right_side_pass_vt(0, c_row):
+			return
+	else:
+		if left_side_pass_upwards(current_selection_vec2.x, current_selection_vec2.y):
+			return
+		if right_side_pass_vt(0, c_col):
+			return
+		if left_side_pass_upwards(t_rows - 1, t_cols - 1):
+			return
+		# if left_side_pass_vt(0, c_row):
+		# 	return
+	
+	print("End nothing found")
+	pass
+
+func target_selection_counter_clockwise():
+	print("Counter Clockwise")
+	setup_quadrant_vars()
+	# print(c_row, current_selection_vec2.x, c_col, current_selection_vec2.y)
+	
+	if c_col <= current_selection_vec2.y:
+		print("Right")
+		if right_side_pass_upwards(current_selection_vec2.x, t_cols - 1):
+		# if right_side_pass_upwards(current_selection_vec2.x, current_selection_vec2.y):
+			return
+		# print_debug("Left Down")
+		if left_side_pass_vt(0, c_col):
+			return
+		if right_side_pass_upwards(t_rows - 1, t_cols - 1):
+			return
+	else:
+		print("Left")
+		if left_side_pass_vt(current_selection_vec2.x, current_selection_vec2.y):
+			return
+		if right_side_pass_upwards(t_rows - 1, t_cols - 1):
+			return
+		if left_side_pass_vt(0, 0):
+			return
+	
+	print("End nothing found")
+	pass
+
+func right_side_clockwise_check():
+	pass
+
+func sort_actors_by_position(a, b) -> bool:
+	print("Sort - ", a.position < b.position)
+	if a.position < b.position:
+		return true
+		
+	return false
+
+func right_side_pass_vt(start_row, start_col) -> bool:
+	setup_quadrant_vars()
+	
+	var r = start_row
+	var c = start_col
+	
+	print(use_range_array_rep, "\n")
+	var check_pos
+	
+	while r < t_rows:
+		while c < t_cols:
+			if use_range_array_rep[r][c] == 1:
+				if current_selection_vec2 == Vector2(r, c):
+					print("Currently Selected - Skip - ", current_selection_vec2)
+					c += 1
+					continue
+				
+				print("row ", r, " col ",  c)
+				
+				check_pos = get_tile_adjustment(r, c_row, c, c_col)
+				print(check_pos)
+				
+				for child in target_node_children:
+					print(check_pos, " ", Vector2(ac_pos.x + check_pos.x, ac_pos.y + check_pos.y), " ", child.position)
+					if Vector2(ac_pos.x + check_pos.x, ac_pos.y + check_pos.y) == child.position:
+						set_and_save_new_target_selection(r, c, child)
+						return true
+				
+			c += 1
+			
+		if start_col >= c_col:
+			c = c_col
+		else:
+			c = 0
+		
+		r += 1
+	
+	print("escape")
+	return false
+
+func left_side_pass_vt(start_row, start_col) -> bool:
+	setup_quadrant_vars()
+	
+	var r = start_row
+	var c = start_col
+	
+	print(use_range_array_rep, "\n")
+	var check_pos
+	
+	while r < t_rows:
+		while c >= 0:
+			if use_range_array_rep[r][c] == 1:
+				if current_selection_vec2 == Vector2(r, c):
+					print("Currently Selected - Skip - ", current_selection_vec2)
+					c -= 1
+					continue
+				
+				print("row ", r, " col ",  c)
+				
+				check_pos = get_tile_adjustment(r, c_row, c, c_col)
+				print(check_pos)
+				
+				for child in target_node_children:
+					print(check_pos, " ", Vector2(ac_pos.x + check_pos.x, ac_pos.y + check_pos.y), " ", child.position)
+					if Vector2(ac_pos.x + check_pos.x, ac_pos.y + check_pos.y) == child.position:
+						set_and_save_new_target_selection(r, c, child)
+						return true
+				
+			c -= 1
+			
+		
+		c = c_col
+		r += 1
+	
+	print("escape")
+	return false
+
+func left_side_pass_upwards(start_row, start_col) -> bool:
+	setup_quadrant_vars()
+	
+	var r = start_row
+	var c = start_col
+	
+	print(use_range_array_rep, "\n")
+	var check_pos
+	
+	while r >= 0:
+		while c >= 0:
+			print(r," ", c)
+			if use_range_array_rep[r][c] == 1:
+				if current_selection_vec2 == Vector2(r, c):
+					print("Currently Selected - Skip - ", current_selection_vec2)
+					c -= 1
+					continue
+				
+				print("row ", r, " col ",  c)
+				
+				check_pos = get_tile_adjustment(r, c_row, c, c_col)
+				print(check_pos)
+				
+				for child in target_node_children:
+					print(check_pos, " ", Vector2(ac_pos.x + check_pos.x, ac_pos.y + check_pos.y), " ", child.position)
+					if Vector2(ac_pos.x + check_pos.x, ac_pos.y + check_pos.y) == child.position:
+						set_and_save_new_target_selection(r, c, child)
+						return true
+				
+			c -= 1
+			
+		
+		c = c_col
+		r -= 1
+		print(r, " ", c, " asd ", 0)
+	
+	print("escape")
+	return false
+
+func right_side_pass_upwards(start_row, start_col) -> bool:
+	setup_quadrant_vars()
+	
+	var r = start_row
+	var c = start_col
+	
+	print(use_range_array_rep, "\n")
+	var check_pos
+	
+	while r >= 0:
+		while c >= c_col:
+			print(r," ", c)
+			if use_range_array_rep[r][c] == 1:
+				if current_selection_vec2 == Vector2(r, c):
+					print("Currently Selected - Skip - ", current_selection_vec2)
+					c -= 1
+					continue
+				
+				print("row ", r, " col ",  c)
+				
+				check_pos = get_tile_adjustment(r, c_row, c, c_col)
+				print(check_pos)
+				
+				for child in target_node_children:
+					print(check_pos, " ", Vector2(ac_pos.x + check_pos.x, ac_pos.y + check_pos.y), " ", child.position)
+					if Vector2(ac_pos.x + check_pos.x, ac_pos.y + check_pos.y) == child.position:
+						set_and_save_new_target_selection(r, c, child)
+						return true
+				
+			c -= 1
+		
+		c = start_col
+		r -= 1
+		print(r, " ", c, " asd ", 0)
+	
+	print("escape")
+	return false
+
+func right_side_pass_vt_old() -> bool:
+	setup_quadrant_vars()
+	
+	var r = 0
+	var c = 0
+	
+	print(use_range_array_rep, "\n")
+	var check_pos
+	
+	while r < t_rows:
+		while c < t_cols:
+			if use_range_array_rep[r][c] == 1:
+				if current_selection_vec2 == Vector2(r, c):
+					print("Currently Selected - Skip - ", current_selection_vec2)
+					c += 1
+					continue
+				
+				print("row ", r, " col ",  c)
+				
+				check_pos = get_tile_adjustment(r, c_row, c, c_col)
+				print(check_pos)
+				
+				for child in target_node_children:
+					print(check_pos, " ", Vector2(ac_pos.x + check_pos.x, ac_pos.y + check_pos.y), " ", child.position)
+					if Vector2(ac_pos.x + check_pos.x, ac_pos.y + check_pos.y) == child.position:
+						set_and_save_new_target_selection(r, c, child)
+						return true
+				
+			c += 1
+			
+		c = 0
+		r += 1
+	
+	print("escape")
+	return false
+
+func get_tile_adjustment(current_row, center_row, current_col, center_col) -> Vector2:
+	return Vector2(get_tile_position_adjustment(current_col, center_col), 
+	get_tile_position_adjustment(current_row, center_row))
+
+func get_tile_position_adjustment(cur_rc, center_rc) -> int:
+	print(cur_rc, " ", center_rc)
+	if cur_rc < center_rc:
+		return ((center_rc - cur_rc) * tile_size) * -1
+	elif cur_rc > center_rc:
+		return (cur_rc - center_rc) * tile_size
+	else:
+		return 0 * tile_size
+
+
+func setup_quadrant_vars() -> void:
+	use_range_array_rep = item_use_range.get_use_range_array_representation()
+	ac_pos = Singleton_Game_GlobalBattleVariables.currently_active_character.position
+	
+	# total rows and columns
+	t_rows = use_range_array_rep.size()
+	t_cols = use_range_array_rep[0].size()
+	
+	# center column and row
+	c_col = floor(t_cols / 2)
+	c_row = floor(t_rows / 2)
+	
+	row_tiles_movement = 0
+	col_tiles_movement = 0
+	
+	check_pos = Vector2(0, 0)
+
+func set_and_save_new_target_selection(current_row, current_col, child) -> void: 
+	current_selection_vec2 = Vector2(current_row, current_col)
+	print("Found - Current Select - ", current_selection_vec2)
+	target_range.draw_cursor_at_position(child.position)
+	Singleton_Game_GlobalBattleVariables.currently_selected_actor = child
+	Singleton_Game_GlobalBattleVariables.battle_base.s_show_target_actor_micro()
