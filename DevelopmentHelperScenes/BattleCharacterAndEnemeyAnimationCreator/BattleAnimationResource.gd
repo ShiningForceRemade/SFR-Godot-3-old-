a6d7ea8d923fc12dd82ba9718d9ab4ea521e99f3
feldc <feldc@protonmail.com>
2021-07-21 01:52:28 -0400

    Add: SF1 battle animation resources for all actors in battle 1

--- DevelopmentHelperScenes/BattleCharacterAndEnemeyAnimationCreator/BattleAnimationResource.gd
+++ DevelopmentHelperScenes/BattleCharacterAndEnemeyAnimationCreator/BattleAnimationResource.gd
@@ -14,7 +14,13 @@ export var rect_position_y_offset: int
 export var rect_position_x_size: int
 export var rect_position_y_size: int
 
+# TODO: make this an array and have a way to tie default animations back to it
+# probably better and more flexible than pre setting the animations 
+# leave for demo to be reworked
 export var animation_res: Animation
+
+export var animation_res_idle: Animation
+export var animation_res_attack: Animation
 
 func _ready():
 	pass
