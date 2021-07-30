c95d8342a15c0c8e0a839f32e5878f3b9f569d62
feldc <feldc@protonmail.com>
2021-07-30 18:06:59 -0400

    TEMP: copy over rune knight ai with minor modifcations for remaining enemey actors

--- SF1/Chapters/1/EnemeyLogicTempDemo/RuneKnight.gd
+++ SF1/Chapters/1/EnemeyLogicTempDemo/RuneKnight.gd
@@ -11,6 +11,17 @@ func play_turn(self_arg):
 	
 	print("Singleton_Game_GlobalBattleVariables.field_logic_node.turn_number ", Singleton_Game_GlobalBattleVariables.field_logic_node.turn_number)
 	
+	if Singleton_Game_GlobalBattleVariables.field_logic_node.turn_number == 1:
+		for _i in range(4):
+			pself.random_move_direction(2)
+			yield(pself, "signal_move_direction_completed")
+
+		pself.animationPlayer.play("DownMovement")
+		emit_signal("signal_logic_completed")
+		pself.internal_call_complete()
+		
+		return
+	
 	####
 	
 	var res = pself.is_character_actor_within_attack_range()
@@ -69,36 +80,6 @@ func play_turn(self_arg):
 	pself.internal_call_complete()
 	
 	return
-	#####
-	
-#	if Singleton_Game_GlobalBattleVariables.field_logic_node.turn_number == 1:
-#		for _i in range(4):
-#			pself.random_move_direction(2)
-#			yield(pself, "signal_move_direction_completed")
-#
-#		pself.animationPlayer.play("DownMovement")
-#
-#		emit_signal("signal_logic_completed")
-#		pself.internal_call_complete()
-#	else:
-#		# yield(pself.get_tree().create_timer(0.1), "timeout")
-#
-#		var res = pself.is_character_actor_within_attack_range()
-#		if res != Vector2.ZERO:
-#			Singleton_Game_GlobalBattleVariables.target_selection_node.setup_use_range_and_target_range_selection_enemey_static()
-#			Singleton_Game_GlobalBattleVariables.target_selection_node.target_range.draw_cursor_at_position(res)
-#			pself.internal_attack_actor_found()
-#
-#			emit_signal("signal_logic_completed")
-#			pself.internal_call_complete()
-#		else:
-#
-#			yield(pself.get_tree().create_timer(0.3), "timeout")
-#			pself.internal_call_complete()
-	
-	# yield(pself.get_tree().create_timer(0.3), "timeout")
-	# pself.internal_call_complete()
-
 
 
 func go_to_target(target_node_arg):
