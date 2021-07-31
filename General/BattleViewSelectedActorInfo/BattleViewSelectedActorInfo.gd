405dc0b27b9112786ede70eb8bcfcf97636ad67f
feldc <feldc@protonmail.com>
2021-07-31 14:45:05 -0400

    Clean: start unifing actor view information on cursor focus selection

--- General/BattleViewSelectedActorInfo/BattleViewSelectedActorInfo.gd
+++ General/BattleViewSelectedActorInfo/BattleViewSelectedActorInfo.gd
@@ -5,7 +5,7 @@ onready var spellInfoNode = preload("res://General/BattleViewSelectedActorInfo/S
 
 var battle_view_selected_actor_info_menu_active: bool = false
 
-# move this to a better place maybe a separate data script
+# TODO: move this to a better place maybe a separate data script
 # singleton seems like overkill
 # but having it here and in the character base route seems prone to issues
 const char_class_array = ["SDMN", "KNT",
@@ -54,10 +54,11 @@ func _input(event):
 			Singleton_Game_GlobalBattleVariables.cursor_root_ref.active = true
 			
 
-func display_character_info():
+func display_character_info() -> void:
 	var actor = Singleton_Game_GlobalBattleVariables.selected_actor.get_node("CharacterRoot")
 	
 	$PortraitSprite.show()
+	# TODO: create protrait get function that checks if unpromtoed or promoted or other
 	$PortraitSprite.texture = actor.texture_protrait
 	
 	$PortraitNinePatchRect.show()
@@ -67,51 +68,16 @@ func display_character_info():
 	$StatNinePatchRect/NameLabel.text = str(actor.character_name)
 	
 	$StatNinePatchRect/LevelLabel.text = str(actor.level)
-	$StatNinePatchRect/HPLabel.text = str(actor.HP_Current) + "/" + str(actor.HP_Total)
-	$StatNinePatchRect/MPLabel.text = str(actor.MP_Current) + "/" + str(actor.MP_Total)
 	$StatNinePatchRect/ExpLabel.text = str(actor.experience_points)
-	
-	$StatNinePatchRect/AttackLabel.text = str(actor.get_attack()) # str(actor.attack)
-	$StatNinePatchRect/DefenseLabel.text = str(actor.defense)
-	$StatNinePatchRect/MoveLabel.text = str(actor.move)
-	$StatNinePatchRect/AgilityLabel.text = str(actor.agility)
 	
 	$CharacterNinePatchRect/KillsAmountLabel.text = str(actor.enemey_kills)
 	$CharacterNinePatchRect/DefeatAmountLabel.text = str(actor.times_defeated)
 	
-	print(actor)
-	var inventory_item_size = actor.inventory_items_id.size()
-	print(inventory_item_size)
-	
-	if inventory_item_size == 0:
-		$StatNinePatchRect/ItemsNothingStaticLabel.show()
-		$StatNinePatchRect/InventoryVBoxContainer.hide()
-	else:
-		$StatNinePatchRect/ItemsNothingStaticLabel.hide()
-		$StatNinePatchRect/InventoryVBoxContainer.show()
-		
-		for n in range(inventory_item_size):
-			var itemInfoN = itemInfoNode.instance()
-			itemInfoN.init_item_micro_info(actor.inventory_items_id[n].texture, actor.inventory_items_id[n].item_name, actor.is_item_equipped[n])
-			$StatNinePatchRect/InventoryVBoxContainer.add_child(itemInfoN)
-	
-	var spells_size = actor.spells_id.size()
-	print(spells_size)
-	
-	if spells_size == 0:
-		$StatNinePatchRect/MagicNothingStaticLabel.show()
-		$StatNinePatchRect/SpellsVBoxContainer.hide()
-	else:
-		$StatNinePatchRect/MagicNothingStaticLabel.hide()
-		$StatNinePatchRect/SpellsVBoxContainer.show()
-		
-		for n in range(spells_size):
-			var spellInfoN = spellInfoNode.instance()
-			spellInfoN.init_spell_micro_info(actor.spells_id[n])
-			$StatNinePatchRect/SpellsVBoxContainer.add_child(spellInfoN)
+	display_actor_info(actor)
 
 
-func display_enemey_info():
+
+func display_enemey_info() -> void:
 	var actor = Singleton_Game_GlobalBattleVariables.selected_actor.get_node("EnemeyRoot")
 	
 	# TODO: when adding boss enemeies with sprites add a check to hide or display similar to character info minus gold display
@@ -124,18 +90,25 @@ func display_enemey_info():
 	$StatNinePatchRect/NameLabel.text = str(actor.enemey_name)
 	
 	$StatNinePatchRect/LevelLabel.text = str(actor.effective_level)
+	$StatNinePatchRect/ExpLabel.text = "N/A" # str(actor.experience_points)
+
+	$CharacterNinePatchRect/KillsAmountLabel.text = "0" # str(actor.enemey_kills)
+	$CharacterNinePatchRect/DefeatAmountLabel.text = "0" # str(actor.times_defeated)
+	
+	display_actor_info(actor)
+
+
+func display_actor_info(actor) -> void:
+	# Stats
 	$StatNinePatchRect/HPLabel.text = str(actor.HP_Current) + "/" + str(actor.HP_Total)
 	$StatNinePatchRect/MPLabel.text = str(actor.MP_Current) + "/" + str(actor.MP_Total)
-	$StatNinePatchRect/ExpLabel.text = "N/A" # str(actor.experience_points)
 	
 	$StatNinePatchRect/AttackLabel.text = str(actor.get_attack()) # str(actor.attack)
 	$StatNinePatchRect/DefenseLabel.text = str(actor.defense)
 	$StatNinePatchRect/MoveLabel.text = str(actor.move)
 	$StatNinePatchRect/AgilityLabel.text = str(actor.agility)
 	
-	$CharacterNinePatchRect/KillsAmountLabel.text = "0" # str(actor.enemey_kills)
-	$CharacterNinePatchRect/DefeatAmountLabel.text = "0" # str(actor.times_defeated)
-	
+	# Inventory
 	print(actor)
 	var inventory_item_size = actor.inventory_items_id.size()
 	print(inventory_item_size)
@@ -152,6 +125,7 @@ func display_enemey_info():
 			itemInfoN.init_item_micro_info(actor.inventory_items_id[n].texture, actor.inventory_items_id[n].item_name, actor.is_item_equipped[n])
 			$StatNinePatchRect/InventoryVBoxContainer.add_child(itemInfoN)
 	
+	# Spells
 	var spells_size = actor.spells_id.size()
 	print(spells_size)
 	
@@ -166,5 +140,3 @@ func display_enemey_info():
 			var spellInfoN = spellInfoNode.instance()
 			spellInfoN.init_spell_micro_info(actor.spells_id[n])
 			$StatNinePatchRect/SpellsVBoxContainer.add_child(spellInfoN)
-			
-	pass
