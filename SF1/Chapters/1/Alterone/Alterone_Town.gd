927a16aef77101d4a6f9867d3c77ff96680087cc
feldc <feldc@protonmail.com>
2022-05-30 23:29:22 -0400

    Clean: SF1 C1 Alterone town scene and tilemap

--- SF1/Chapters/1/Alterone/Alterone_Town.gd
+++ SF1/Chapters/1/Alterone/Alterone_Town.gd
@@ -8,15 +8,11 @@ var using_original_tiles: bool = true
 
 # original tilesets
 var zero_tres = load("res://SF1/TileSet_Resources/Original/0.tres")
-var seven_tres = load("res://SF1/TileSet_Resources/Original/7.tres")
-var eight_tres = load("res://SF1/TileSet_Resources/Original/8.tres")
 var nine_tres = load("res://SF1/TileSet_Resources/Original/9.tres")
 var ten_tres = load("res://SF1/TileSet_Resources/Original/10.tres")
 
 # upgrade Ivan Cal tilesets
 var zero_ic_tres = load("res://SF1/TileSet_Resources/Ivan_Cal_Graphic_Upgrade/0.tres")
-var seven_ic_tres = load("res://SF1/TileSet_Resources/Ivan_Cal_Graphic_Upgrade/7.tres")
-var eight_ic_tres = load("res://SF1/TileSet_Resources/Ivan_Cal_Graphic_Upgrade/8.tres")
 var nine_ic_tres = load("res://SF1/TileSet_Resources/Ivan_Cal_Graphic_Upgrade/9.tres")
 var ten_ic_tres = load("res://SF1/TileSet_Resources/Ivan_Cal_Graphic_Upgrade/10.tres")
 
@@ -28,17 +24,9 @@ func _input(event):
 	if event.is_action_released("ui_page_down"):
 		print("Swap Tilesets")
 		if using_original_tiles:
-			TilemapSceneRoot.get_node("Tile Layer 1").tile_set = zero_ic_tres
-			TilemapSceneRoot.get_node("Tile Layer 1/7").tile_set = seven_ic_tres
-			TilemapSceneRoot.get_node("Tile Layer 1/8").tile_set = eight_ic_tres
-			TilemapSceneRoot.get_node("Tile Layer 1/9").tile_set = nine_ic_tres
-			TilemapSceneRoot.get_node("Tile Layer 1/10").tile_set = ten_ic_tres
-			
 			TilemapSceneRoot.get_node("Copy of Tile Layer 1").tile_set = zero_ic_tres
 			TilemapSceneRoot.get_node("Copy of Tile Layer 1/9").tile_set = nine_ic_tres
 			TilemapSceneRoot.get_node("Copy of Tile Layer 1/10").tile_set = ten_ic_tres
-			
-			TilemapSceneRoot.get_node("Roof").tile_set = zero_ic_tres
 			
 			TilemapRoofSceneRoot.get_node("TopLeftHouse").tile_set = zero_ic_tres
 			TilemapRoofSceneRoot.get_node("TopRightHouse").tile_set = zero_ic_tres
@@ -52,17 +40,9 @@ func _input(event):
 			
 			using_original_tiles = false
 		else:
-			TilemapSceneRoot.get_node("Tile Layer 1").tile_set = zero_tres
-			TilemapSceneRoot.get_node("Tile Layer 1/7").tile_set = seven_tres
-			TilemapSceneRoot.get_node("Tile Layer 1/8").tile_set = eight_tres
-			TilemapSceneRoot.get_node("Tile Layer 1/9").tile_set = nine_tres
-			TilemapSceneRoot.get_node("Tile Layer 1/10").tile_set = ten_tres
-			
 			TilemapSceneRoot.get_node("Copy of Tile Layer 1").tile_set = zero_tres
 			TilemapSceneRoot.get_node("Copy of Tile Layer 1/9").tile_set = nine_tres
 			TilemapSceneRoot.get_node("Copy of Tile Layer 1/10").tile_set = ten_tres
-			
-			TilemapSceneRoot.get_node("Roof").tile_set = zero_tres
 			
 			TilemapRoofSceneRoot.get_node("TopLeftHouse").tile_set = zero_tres
 			TilemapRoofSceneRoot.get_node("TopRightHouse").tile_set = zero_tres
